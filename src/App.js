d08ef24dccca368098db50ebdc2479f5fdfade33
olaru-miruna-paraschiva-30127 <99726825+olaru-miruna-paraschiva-30127@users.noreply.github.com>
2022-11-07 17:08:48 +0200

    4 files modified

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import { BsCheck2Square} from "react-icons/bs";
 import {CgAddR} from "react-icons/cg";
 import {MdOutlineDateRange} from "react-icons/md";
 import Graphic1 from './Graphic1';
+import Graphic from './Graphic';
 import React, { useState } from "react";
 import {TbLayoutDashboard} from "react-icons/tb";
 import {FaMedal} from "react-icons/fa";
 import {HiOutlineBookOpen} from "react-icons/hi";
 import {TbBriefcase} from "react-icons/tb";
 import {FiSettings} from "react-icons/fi";
+import {VscGraph} from "react-icons/vsc";
+import {BsPeople} from "react-icons/bs";
+import {TbCircles} from "react-icons/tb";
 
 export default function App() {
   const [active, setActive] = useState(false);
@@ -40,63 +44,83 @@ export default function App() {
   const h5 = () => {
     sA5(!a5);
   };
+  const text = 'Name';
+  const date = 'Date';
   return (
     <div>
 
-<div>
+
   <nav>
-  <button onClick={() =>{handleClick1(); h1();}} style={{
-       border:active1?'1px solid white': '1px solid purple',
+  <div>
+   <h1> RadicalX</h1>
+   <br/>
+  <button onClick={() =>{ h1();}} style={{
+       border:a1?'1px solid white': '1px solid purple',
        borderRadius: '5px',
       
         height:'45px',
         width:'120px',
-        backgroundColor:active1 && a1? "white" : 'rgb(221,160,221)',
-     color:active1 ?'black':'white'
+        backgroundColor: a1? "white" : 'rgb(221,160,221)',
+     color:a1 ?'black':'white'
      }}  > <TbLayoutDashboard/> Dashboard </button>  <br/>
   <br/>
   
-  <button onClick={() =>{handleClick1(); h1();}} style={{
-       border:active1?'1px solid white': '1px solid purple',
+  <button onClick={() =>{ h2();}} style={{
+       border:a2?'1px solid white': '1px solid purple',
        borderRadius: '5px',
       
         height:'45px',
         width:'120px',
-        backgroundColor:active1 && a1 ? "white" : 'rgb(221,160,221)',
-     color:active1 ?'black':'white'
+        backgroundColor:a2 ? "white" : 'rgb(221,160,221)',
+     color:a2 ?'black':'white'
      }} > <FaMedal/> Apprenterships</button>  <br/>
   <br/>
-  <button onClick={handleClick} style={{
-       border:active?'1px solid white': '1px solid purple',
+  <button onClick={h3} style={{
+       border:a3?'1px solid white': '1px solid purple',
        borderRadius: '5px',
       
         height:'45px',
         width:'120px',
-        backgroundColor:active ? "white" : 'rgb(221,160,221)',
-     color:active ?'black':'white'
+        backgroundColor:a3? "white" : 'rgb(221,160,221)',
+     color:a3 ?'black':'white'
      }}><HiOutlineBookOpen/> Internships</button>  <br/>
   <br/>
-  <button onClick={handleClick} style={{
-       border:active?'1px solid white': '1px solid purple',
+  <button onClick={h4} style={{
+       border:a4?'1px solid white': '1px solid purple',
        borderRadius: '5px',
       
         height:'45px',
         width:'120px',
-        backgroundColor:active ? "white" : 'rgb(221,160,221)',
-     color:active ?'black':'white'
+        backgroundColor:a4 ? "white" : 'rgb(221,160,221)',
+     color:a4 ?'black':'white'
      }}> <TbBriefcase/> Jobs </button>  <br/>
   <br/>
-  <button onClick={handleClick} style={{
-       border:active?'1px solid white': '1px solid purple',
+  <button onClick={h5} style={{
+       border:a5?'1px solid white': '1px solid purple',
        borderRadius: '5px',
       
         height:'45px',
         width:'120px',
-        backgroundColor:active ? "white" : 'rgb(221,160,221)',
-     color:active ?'black':'white'
+        backgroundColor:a5 ? "white" : 'rgb(221,160,221)',
+     color:a5 ?'black':'white'
      }}> <FiSettings/> Settings </button>  <br/>
+     <br/>
+     <br/>
+     <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+     <br/> <br/> <br/> <br/> <br/> <br/> 
+      <button style={{
+       border:'1px solid white',
+       borderRadius: '5px',
+      backgroundImage:'',
+        height:'45px',
+        width:'45px',
+        backgroundColor:"lightgray" ,
+     
+     }}></button> {text}
+
+     </div>
   </nav>
-  </div>
+  
   <div  style={{ display:'flex',
    flexdirection:'row',
   float: 'left',
@@ -221,13 +245,217 @@ export default function App() {
   
   <div>
    <aside>
-   <BsCheck2Square/>
+   <div  style={{  
+  display:'flex',
+    flex:1,
+    height:'50px',
+   flexdirection:'row-reverse',
+  
+   justifyContent:'space-between'
+   }}>
+    <div>Internship Title </div>
+   <div><BsCheck2Square/> Completion  Period</div>
+    <div><BsCheck2Square/> Total Enrolled </div>
+    <div><BsCheck2Square/> Qualified Candidates </div>
+    <div><BsCheck2Square/>&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    </div>
+     </div>
+    
    </aside>
      </div>
-   <div>
-     <article>
-       </article>
+   
+     <article style={{height:'500px'}}>
+ 
+     <div  style={{  
+  display:'flex',
+    flex:1,
+    height:'50px',
+   flexdirection:'row-reverse',
+  
+   justifyContent:'space-between'
+   }}>
+     <div>
+     <div>
+       <h4>Product Design GVI</h4>
+       <p style={{fontSize:'10px',color:'gray'}}>
+       Amet mimin mollit non deserunt
+         <br/>est sit aliqua dolor do amet sint.
+       </p>
+      
      </div>
+     <div>
+   
+       <h4>Product Design GVI</h4>
+       <p style={{fontSize:'10px',color:'gray'}}>
+       Amet mimin mollit non deserunt
+         <br/>est sit aliqua dolor do amet sint.
+       </p>
+      
+     </div>
+     <div>
+   
+       <h4>Product Design GVI</h4>
+       <p style={{fontSize:'10px',color:'gray'}}>
+       Amet mimin mollit non deserunt
+         <br/>est sit aliqua dolor do amet sint.
+       </p>
+       
+     </div>
+     <div>
+   
+       <h4>Product Design GVI</h4>
+       <p style={{fontSize:'10px',color:'gray'}}>
+       Amet mimin mollit non deserunt
+         <br/>est sit aliqua dolor do amet sint.
+       </p>
+       
+     </div>
+     <div>
+   
+       <h4>Product Design GVI</h4>
+       <p style={{fontSize:'10px',color:'gray'}}>
+       Amet mimin mollit non deserunt
+         <br/>est sit aliqua dolor do amet sint.
+       </p>
+       <br/>
+     </div>
+     </div>
+     <div>
+     <div>
+     <h4>120 days</h4>
+     <p style={{fontSize:'10px',color:'gray'}}>(created on {date})
+     <br/>
+     <br/>
+     </p>
+   </div>
+   <div>
+     <h4>120 days</h4>
+     <p style={{fontSize:'10px',color:'gray'}}>(created on {date})
+     <br/>
+     <br/>
+     </p>
+   </div>
+   <div>
+     <h4>120 days</h4>
+     <p style={{fontSize:'10px',color:'gray'}}>(created on {date})
+     <br/>
+     <br/>
+     </p>
+   </div>
+   <div>
+     <h4>120 days</h4>
+     <p style={{fontSize:'10px',color:'gray'}}>(created on {date})
+     <br/>
+     <br/>
+     </p>
+   </div>
+   <div>
+     <h4>120 days</h4>
+     <p style={{fontSize:'10px',color:'gray'}}>(created on {date})
+     <br/>
+     <br/>
+     </p>
+   </div>
+   </div>
+   <div>
+   <div>
+     <p style={{backgroundColor:'rgb(221,160,221)',color:'purple',width:'55px',height:'23px',padding:'3px', borderRadius: '5px'}}>20,000</p>
+     </div>
+     <br/>
+     <br/>
+     <div>
+     <p style={{backgroundColor:'rgb(221,160,221)',color:'purple',width:'55px',height:'23px',padding:'3px', borderRadius: '5px'}}>20,000</p>
+     </div>
+     <br/>
+     <br/>
+     <div>
+     <p style={{backgroundColor:'rgb(221,160,221)',color:'purple',width:'55px',height:'23px',padding:'3px', borderRadius: '5px'}}>20,000</p>
+     </div>
+     <br/>
+     <br/>
+     <div>
+     <p style={{backgroundColor:'rgb(221,160,221)',color:'purple',width:'55px',height:'23px',padding:'3px', borderRadius: '5px'}}>20,000</p>
+     </div>
+     <br/>
+     <br/>
+     <div>
+     <p style={{backgroundColor:'rgb(221,160,221)',color:'purple',width:'55px',height:'23px',padding:'3px', borderRadius: '5px'}}>20,000</p>
+     </div>
+     </div>
+   <div>
+     <Graphic/>
+     <Graphic/>
+     <Graphic/>
+     <Graphic/>
+     <Graphic/>
+   </div>
+   <div>
+   <div  style={{  
+  display:'flex',
+    flex:1,
+    height:'50px',
+   flexdirection:'row-reverse',
+  color:'purple',
+  width:'100px',
+  height:'100px',
+   justifyContent:'space-between'
+   }}> <VscGraph/><BsPeople/><TbCircles/>
+     </div>
+     <div  style={{  
+  display:'flex',
+    flex:1,
+    height:'50px',
+   flexdirection:'row-reverse',
+  color:'purple',
+  width:'100px',
+  height:'100px',
+   justifyContent:'space-between'
+   }}> <VscGraph/><BsPeople/><TbCircles/>
+     </div>
+     <div  style={{  
+  display:'flex',
+    flex:1,
+    height:'50px',
+   flexdirection:'row-reverse',
+  color:'purple',
+  width:'100px',
+  height:'100px',
+   justifyContent:'space-between'
+   }}> <VscGraph/><BsPeople/><TbCircles/>
+     </div>
+     <div  style={{  
+  display:'flex',
+    flex:1,
+    height:'50px',
+   flexdirection:'row-reverse',
+  color:'purple',
+  width:'100px',
+  height:'100px',
+   justifyContent:'space-between'
+   }}> <VscGraph/><BsPeople/><TbCircles/>
+     </div>
+     <div  style={{  
+  display:'flex',
+    flex:1,
+    height:'50px',
+   flexdirection:'row-reverse',
+  color:'purple',
+  width:'100px',
+  height:'100px',
+   justifyContent:'space-between'
+   }}> <VscGraph/><BsPeople/><TbCircles/>
+     </div>
+   
+     </div>
+   
+     </div>
+     
+       </article>
+    
    
    
     </div>
--- src/Graphic.js
+++ src/Graphic.js
@@ -1,30 +1,52 @@
 
 import {Bar} from 'react-chartjs-2';
 import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 export default function Graphic(){
   return(
-    <div  style={{height:'100px',width:'200px',padding:'3px'}}>
+    <div  style={{height:'85px',width:'200px',padding:'3px'}}>
   <Bar
-          data={{labels: ['January', 'February', 'March',
-          'April', 'May'],
+  
+          data={{labels: ['', '', '',
+          '', ''],
  datasets: [
-   {
-     label: 'Rainfall',
+   { 
+     label: '',
      backgroundColor: 'rgba(75,192,92,1)',
-     borderColor: 'rgba(0,0,0,1)',
-     borderWidth: 2,
+     
+     
      data: [65, 59, 80, 81, 56]
 
    }
  ]}}
           options={{
+            
             title:{
               display:true,
               text:'Average Rainfall per month',
               fontSize:20
             },
             legend:{
-              display:true,
+              display:false,
               position:'right'
             }
           }}
